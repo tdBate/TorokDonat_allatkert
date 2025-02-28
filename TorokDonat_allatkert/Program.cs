c458b6b513c38e69ea601cc0ece1095c8e8ed172
Torok Donat <diak@petrik.hu>
2025-02-28 11:07:50 +0100

    Állatkert feltöltése

--- TorokDonat_allatkert/Program.cs
+++ TorokDonat_allatkert/Program.cs
@@ -6,14 +6,27 @@
 		{
 			Random rnd = new Random();
 
-			Allat p1 = new Allat("Ponty",4,2);
-			Allat g1 = new Allat("Görög teknős", 1, 3);
-			Allat n1 = new Allat("Nyúl", 5, 2);
-			Allat k1 = new Allat("Kaméleon", 2, 1);
-			Allat j1 = new Allat("Jegesmedve",6,5);
+			//Allat[] allatok = {g1, n1, k1, j1 };
+			List<Allat> allatok = new List<Allat>();
+			string[] allatNevek = { "Ponty", "Görög teknős", "Nyúl", "Kaméleon", "Jegesmedve" };
 
-			int allatkertMeret = rnd.Next(5,16);
+			int allatkertMaxMeret = rnd.Next(5,16);
 
-		}
+			int allatkertMeret = 0;
+			bool marade = true;
+			while (marade)
+			{
+				string allatNev = allatNevek[rnd.Next(0, allatNevek.Length)];
+				int meret= rnd.Next(1, 5);
+				if (allatkertMeret + meret <= allatkertMaxMeret)
+				{
+					allatok.Add(new Allat(allatNev, rnd.Next(1, 10), meret));
+					allatkertMeret += meret;
+				}
+				else { marade = false; }
+			}
+
+			
+        }
 	}
 }
