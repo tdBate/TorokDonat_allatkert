5766f2165de6f3b256251caa0e74444d1271d003
Torok Donat <diak@petrik.hu>
2025-02-28 10:26:31 +0100

    Ponty osztály kész

---
+++ TorokDonat_allatkert/Ponty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorokDonat_allatkert
+{
+	internal class Ponty
+	{
+		private string nev;
+		private int sebesseg;
+		private int meret;
+
+		public Ponty(string nev, int sebesseg, int meret)
+		{
+			this.nev = nev;
+			this.sebesseg = sebesseg;
+			this.meret = meret;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+		public int Meret { get => meret; set => meret = value; }
+	}
+}
