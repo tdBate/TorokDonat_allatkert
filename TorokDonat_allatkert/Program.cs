5d2f961f9de704879e5f4e02188e8b0a93a99e08
Torok Donat <diak@petrik.hu>
2025-02-28 10:48:28 +0100

    Allat osztaly kesz, átmódosítottam a példányosítást

--- TorokDonat_allatkert/Program.cs
+++ TorokDonat_allatkert/Program.cs
@@ -4,7 +4,16 @@
 	{
 		static void Main(string[] args)
 		{
-			
+			Random rnd = new Random();
+
+			Allat p1 = new Allat("Ponty",4,2);
+			Allat g1 = new Allat("Görög teknős", 1, 3);
+			Allat n1 = new Allat("Nyúl", 5, 2);
+			Allat k1 = new Allat("Kaméleon", 2, 1);
+			Allat j1 = new Allat("Jegesmedve",6,5);
+
+			int allatkertMeret = rnd.Next(5,16);
+
 		}
 	}
 }
